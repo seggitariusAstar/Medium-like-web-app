254878d4d61ee5a4f0c63387d3bb912fd57831d3
seggitariusAstar <harshtyagiop229@gmail.com>
2024-03-09 00:36:28 +0530

    Frontend for the Medium application

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { Signin } from './pages/Signin'
+import { Signup } from './pages/Signup'
+import { Blogs } from './pages/Blogs'
+import { Blog } from './pages/Blog'
+import { Publish } from './pages/Publish'
+function App() {
+
+
+  return (
+    <>
+   <BrowserRouter>
+      <Routes>
+        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='/signin' element={<Signin/>}></Route>
+        <Route path='/blog/:id' element={<Blog/>}></Route>
+        <Route path='/blogs' element={<Blogs/>}></Route>
+        <Route path='/publish'element={<Publish/>}></Route>
+        
+        
+      </Routes>
+    
+     
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
